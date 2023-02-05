8962f9ffb812edda6d39baebb883f5951603b695
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-02-06 01:10:32 +0600

    Load data (Read Data) from mongoDB by :id

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
 const app = express();
 // process.env a jodi kono port thake tahole seta use koro nahole 5000 use koro
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 // middle wares
 app.use(cors());
@@ -24,7 +24,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
 
+        // db to get all data
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // dynamically get a info on db
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
